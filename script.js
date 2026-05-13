a21933a7278064af4a71073e076cc55c5cf7bdcd
ㅤBoltznn <pedroka10kas@gmail.com>
2026-05-12 21:43:51 -0300

    :broom: cleanup: Limpando para o Ex2.

--- script.js
+++
@@ -1,8 +0,0 @@
-let mensagem = "";
-
-//For foi utlizado para o looping de repetição, pois irá fazer um looping onde terá um número exato de repetições
-for (let i = 10; i >= 1; i -= 1) {
-  mensagem = `${mensagem} ${i}`;
-
-  alert(mensagem);
-}
