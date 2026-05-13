b930d4b8b2847cac04a65b6356633f9dd1862b27
ㅤBoltznn <pedroka10kas@gmail.com>
2026-05-12 21:45:06 -0300

    :broom: cleanup: Limpando para o Ex3.

--- script.js
+++
@@ -1,9 +0,0 @@
-let i = 1;
-let soma = 0;
-
-//While foi utlizado para o looping, porque enquanto "i" for menor que "11" o looping irá se repetir
-while (i < 11) {
-  soma = soma + i;
-  i += 1;
-  alert(`${soma}`);
-}
