b4badc8e8d90bf01dbd175ded85e4930a129dfe2
I1HN <pedroka10kas@gmail.com>
2026-05-13 07:47:28 -0300

    :bulb: docs: Arrumando comentário.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let mensagem = "";
 
-//For foi utlizado para o looping de repetição, pois irá fazer um looping onde terá um número exato de repetições
+//For foi utilizado para o looping de repetição, pois irá fazer um looping onde terá um número exato de repetições
 for (let i = 10; i >= 1; i -= 1) {
   mensagem = `${mensagem} ${i}`;
 
