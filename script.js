e362128a445266f0b1ccc67b73de43bf4ae5a78a
I1HN <pedroka10kas@gmail.com>
2026-05-13 07:45:45 -0300

    :recycle: refactor: Excluindo variável inutilizada.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 let numUm = Number(prompt("Digite um número para ver a sua tabuada!"));
-let mensagem = "";
 
 if (isNaN(numUm)) {
-    alert("Número inválido!");
-}
-else {
-    for (let i = 1; i <= 10; i += 1) {
-        let resultado = numUm * i;
+  alert("Número inválido!");
+} else {
+  for (let i = 1; i <= 10; i += 1) {
+    let resultado = numUm * i;
 
-        alert(`${numUm} x ${i} = ${resultado}`);
-    }
+    alert(`${numUm} x ${i} = ${resultado}`);
+  }
 }
