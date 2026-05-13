c40bc5f5b352856b63b2acc316ccfa3660c197fa
ㅤBoltznn <pedroka10kas@gmail.com>
2026-05-12 21:47:24 -0300

    :broom: cleanup: Limpando para o Ex5.

--- script.js
+++
@@ -1,8 +0,0 @@
-let menu;
-//do para realizar prompt antes de escolher a opcão.
-
-do {
-  menu = Number(prompt(" 1- Inserir produto | 2- Listar produtos | 3- Sair "));
-//while para  encerrar o sistema caso o usuário digitar 3.
-} while (menu != 3);
-alert(`Sistema encerrado.`);
